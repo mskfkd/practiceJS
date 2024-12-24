0b8dfe634acff6edc3fd924b90f26544e3c463b8
mskfkd <fm6096@yahoo.co.jp>
2024-12-24 22:10:31 +0900

    コメント、変数修正

--- fortuneTelling.js
+++ fortuneTelling.js
@@ -1,7 +1,7 @@
-// 名前を入力すると運勢結果が表示される
+// ボタンをクリックすると運勢結果が表示される
 
 // 表示される入力欄
-let fortuneTelling = document.getElementById("fortuneTelling");
+const fortuneTelling = document.getElementById("fortuneTelling");
 
 // 運勢結果ストック
 const result = [
@@ -18,9 +18,9 @@ const result = [
 /* ランダムで運勢結果ストックの中から1つ選択して表示する
 */
 fortuneTelling.addEventListener("click", function() {
-  let resultNum = Math.floor(Math.random() * result.length);
-  let resultText = result[resultNum];
+  const resultNum = Math.floor(Math.random() * result.length);
+  const resultText = result[resultNum];
 
-  let displayFortuneTellingResult = document.getElementById("result");
+  const displayFortuneTellingResult = document.getElementById("result");
   displayFortuneTellingResult.innerHTML = "今日の運勢は「" + resultText + "」だよ!";
 }); 
