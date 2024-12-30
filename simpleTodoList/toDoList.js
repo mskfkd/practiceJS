a4316062eed48fe004617560b8ef641212fe385f
mskfkd <fm6096@yahoo.co.jp>
2024-12-30 23:51:06 +0900

    入力フォームからタスク追加後の表示不具合修正、タスク追加後フォームの内容削除処理追加

--- simpleTodoList/toDoList.js
+++ simpleTodoList/toDoList.js
@@ -2,14 +2,18 @@ const addTodoListButton = document.getElementById("addTodo");
 const todos = [];
 
 addTodoListButton.addEventListener("click", () => {
-  const getTodoContent = document.querySelector("#inputText").value;
+  let getTodoContent = document.querySelector("#inputText").value;
   
-  todos.push(getTodoContent);
-  todos.forEach(todo => {
-    const list = document.querySelector("#displayTodo");
-    let li = document.createElement("li");
-    li.innerHTML = todo;
-    //@@@ todo ↓表示できないので要確認
-    list.appendChild(li);
-  });
+  if (getTodoContent != "") {
+    todos.push(getTodoContent);
+    todos.forEach(todo => {
+      const list = document.querySelector(".displayTodo");
+      let li = document.createElement("li");
+      li.innerHTML = todo;
+      list.appendChild(li);
+    });
+  }
+
+  document.querySelector("#inputText").value = "";
+
 });
