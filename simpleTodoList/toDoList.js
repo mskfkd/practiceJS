a0e76051290bc8dfacd41b6d6a85f4116701b038
mskfkd <fm6096@yahoo.co.jp>
2024-12-30 18:00:29 +0900

    入力欄にタスクを入力し、追加ボタンをクリックすると下に追加したタスクが表示されるよう処理追加

--- simpleTodoList/toDoList.js
+++ simpleTodoList/toDoList.js
@@ -1,1 +1,15 @@
 const addTodoListButton = document.getElementById("addTodo");
+const todos = [];
+
+addTodoListButton.addEventListener("click", () => {
+  const getTodoContent = document.querySelector("#inputText").value;
+  
+  todos.push(getTodoContent);
+  todos.forEach(todo => {
+    const list = document.querySelector("#displayTodo");
+    let li = document.createElement("li");
+    li.innerHTML = todo;
+    //@@@ todo ↓表示できないので要確認
+    list.appendChild(li);
+  });
+});
